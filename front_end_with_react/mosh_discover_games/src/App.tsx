6c0f4a1c98c5de800d537bd1cfed419fed97af46
My name <zichuan.cecilia@gmail.com>
2024-05-30 17:57:10 -0400

    upload

---
+++ front_end_with_react/mosh_discover_games/src/App.tsx
@@ -0,0 +1,42 @@
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items_2 = ["China", "U.S.", "Iceland", "India", "France"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  const onClick = () => {
+    console.log("Clicked");
+  };
+
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <ListGroup
+        items={items_2}
+        heading="Countries"
+        onSelectItem={handleSelectItem}
+      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
--- front_end_with_react/w1 create react app.md
+++ front_end_with_react/w1 create react app.md
@@ -9,3 +9,21 @@ cd myfirstapp
 # start the application
 
 npm start
+
+
+# Create react app using Vite
+npm create vite@latest
+
+cd myreactapp
+
+npm install
+
+npm run dev
+
+# install bootstrap
+npm i boostrap@5.2.3
+
+# then import on main.tsx
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+# get code template from https://getbootstrap.com/
